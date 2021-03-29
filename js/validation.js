9ed22624bd0bd7532471b110bada8cf3e631ac6d
Julio Sergio <juliosergio010302@gmail.com>
2021-03-29 10:41:10 -0300

    Made a function to validate the date

---
+++ js/validation.js
@@ -0,0 +1,23 @@
+const birthDate = document.querySelector('#nascimento')
+
+birthDate.addEventListener('blur', (event) => {
+    validateBirthDate(event.target)
+})
+
+function validateBirthDate(input) {
+    const receivedDate = new Date(input.value)
+    let message = ''
+
+    if (!OlderThan18(receivedDate)) {
+        message = 'Você deve ser maior e 18 anos para se cadastrar'
+    }
+    
+    input.setCustomValidity(message)
+}
+
+function OlderThan18(date) {
+    const currentDate = new Date()
+    const dateOlderThan18 = new Date(date.getUTCFullYear() + 18, date.getUTCMonth(), date.getUTCDate())
+
+    return dateOlderThan18 <= currentDate
+}
